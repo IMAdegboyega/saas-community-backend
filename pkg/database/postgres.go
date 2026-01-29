1110e3c6fa479dc7ab0cd61b9611b2dcb303d915
Marvelous Iretomiwa <Marveltoms2014@gmail.com>
2026-01-29 19:49:59 +0100

    production ready test v8

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -17,10 +17,12 @@ func NewPostgresDB(url string) (*sqlx.DB, error) {
 	}
 
 	// Configure connection pool
+	// Short connection lifetime to avoid prepared statement caching issues
+	// when queries change between deployments
 	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(5)
-	db.SetConnMaxLifetime(5 * time.Minute)
-	db.SetConnMaxIdleTime(1 * time.Minute)
+	db.SetMaxIdleConns(2)                    // Reduced from 5
+	db.SetConnMaxLifetime(1 * time.Minute)   // Reduced from 5 minutes
+	db.SetConnMaxIdleTime(30 * time.Second)  // Reduced from 1 minute
 
 	// Test connection
 	if err := db.Ping(); err != nil {
